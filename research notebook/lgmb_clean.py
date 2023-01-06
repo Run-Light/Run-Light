507def33b472631d785cc6c4b7913e8b1c707be0
Mathieu Laversin <mathieulaversin@outlook.fr>
2023-01-06 09:42:31 +0100

    export_csv_ok

--- research notebook/lgmb_clean.py
+++ research notebook/lgmb_clean.py
@@ -1,16 +1,26 @@
+#Import the libraries
+
 import numpy as np
+import math 
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import normalize
 from lightgbm.sklearn import LGBMRegressor
-
+from sklearn.metrics import mean_squared_error, r2_score
 from Validation.Validation import ExceptOneValidation
+
+#Import and creation of a dataframe(Table)
 
 data=pd.read_csv("data_subject_filtrage_5.csv")
 df=data.copy()
 
+
+
 def get_variation(df):
+    '''
+    df : DataFrame
+    output : DataFrame with variations values in columns'''
     
     x_lum_var=df.x_lumbar-df.x_lumbar.shift(periods=1, fill_value=df.x_lumbar[len(df)-1])
     y_lum_var=df.y_lumbar-df.y_lumbar.shift(periods=1, fill_value=df.y_lumbar[len(df)-1])
@@ -30,20 +40,34 @@ def get_variation(df):
     
     return df.join(df_var)
 
+#Application of the function
+
 df=get_variation(df)
+
+#Drop columns we don't want/ are not really correlated 
 X=df.drop(['index','nb_seance','nb_passage','x_force','z_force','y_force'],axis=1)
+#Target
 y=df['z_force']
+
+#LGMB Regressor model 
 
 lgbmr=LGBMRegressor(boosting_type='goss',random_state=42,n_estimators=300,learning_rate=0.2,n_jobs=-1)
 score_lgbmr,error_lgbmr,predict_lgbmr= ExceptOneValidation(lgbmr,X,y)
 
-import math 
+#Results
 print('MSE:',math.sqrt(pd.Series(error_lgbmr).mean()))
 print('Score:',pd.Series(score_lgbmr).mean()*100,'%')
 
-pd.Series(error_lgbmr).describe()
+#Error 
+print("The error summary for the model : ")
+print(pd.Series(error_lgbmr).describe())
 
-pd.Series(score_lgbmr).describe()
+
+
+print("The score summary for the model : ")
+print(pd.Series(score_lgbmr).describe())
+
+#Plot preprocessing
 
 df['z_pred']=predict_lgbmr
 fig,axes = plt.subplots(20,1,figsize=(20,120))
@@ -53,7 +77,9 @@ for i in range(20):
     df['z_pred'].iloc[(i+2)*5000:(i+2)*5000+5000].reset_index(drop=True).plot(ax=axes[i],title=f'z_pred_{i}')
     df['z_pred'].iloc[(i+3)*5000:(i+3)*5000+5000].reset_index(drop=True).plot(ax=axes[i],title=f'z_pred_{i}')
     df['z_pred'].iloc[(i+4)*5000:(i+4)*5000+5000].reset_index(drop=True).plot(ax=axes[i],title=f'z_pred_{i}')
-    
+
+#Graph printing the results smooth : 
+
 df['z_pred_smooth']=df['z_pred']
 wind=201
 windna=int((wind-1)/2)
@@ -67,9 +93,15 @@ for i in range(20):
     df['z_pred_smooth'].iloc[(i+2)*5000:(i+2)*5000+5000].reset_index(drop=True).plot(ax=axes[i],title=f'z_pred_{i}')
     df['z_pred_smooth'].iloc[(i+3)*5000:(i+3)*5000+5000].reset_index(drop=True).plot(ax=axes[i],title=f'z_pred_{i}')
     df['z_pred_smooth'].iloc[(i+4)*5000:(i+4)*5000+5000].reset_index(drop=True).plot(ax=axes[i],title=f'z_prey_pred_smoothd_{i}')
-    
-from sklearn.metrics import mean_squared_error, r2_score
+
+#Comparison of the result (not smooth/smooth) :
+  
+
 print("MSE:")
 print('Raw',mean_squared_error(df['z_force'],df['z_pred'])**(1/2),'Smooth:',mean_squared_error(df['z_force'],df['z_pred_smooth'])**(1/2))
 print("Score:")
 print('Raw',r2_score(df['z_force'],df['z_pred']),'Smooth:',r2_score(df['z_force'],df['z_pred_smooth']))
+
+#Exportation of the predictions into a csv file :
+
+np.savetxt("Exported_data\data_predict.csv", predict_lgbmr, delimiter=",")
